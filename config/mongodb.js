ef9a971979a77404453da126e8d46615208ef031
Kshitizt2099 <109498859+Kshitizt2099@users.noreply.github.com>
2024-01-08 02:25:23 +0530

    Update mongodb.js

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,21 +1,16 @@
-import { MongoClient } from "mongodb";
-//in url replace localhost by 0.0.0.0
-const url='mongodb+srv://kshitijtawra2099:AlanWake2023@cluster0.djh4imd.mongodb.net/?retryWrites=true&w=majority';
-let client;
-export const connecttomondodb=()=>{
-    //it returns promise so we can use then cathch
-    MongoClient.connect(url).then(curr=>
-            {
-                client=curr;
-                console.log("it is connected");
-            }        
-        
-        ).catch((err)=>{
-           console.log(err);
-        }
-        )
-}
-export const getDB=()=>{
-       return client.db()
-}
+import mongoose from "mongoose";
+const url="mongodb+srv://kshitijtawra2099:AlanWake2023@cluster0.djh4imd.mongodb.net/?retryWrites=true&w=majority"
 
+export const connecttodb=async()=>{
+    
+    try{
+           await mongoose.connect(url,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+           });
+    }
+    catch(err){
+        console.log(err);
+
+    }
+}
